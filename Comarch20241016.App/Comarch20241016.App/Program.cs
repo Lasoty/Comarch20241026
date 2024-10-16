6e25794ef5f369f9d89c72a7202d0860cbb1f8c5
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-10-16 15:44:59 +0200

    Kalkulator

--- Comarch20241016.App/Comarch20241016.App/Program.cs
+++ Comarch20241016.App/Comarch20241016.App/Program.cs
@@ -17,18 +17,45 @@ namespace Comarch20241016.App
         /// <returns></returns>
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj, jak masz na imię? ");
+            Console.WriteLine("KALKULATOR 1.0");
+            Console.WriteLine("  1. Dodawanie");
+            Console.WriteLine("  2. Odejmowanie");
 
-            string imie;
-            imie = Console.ReadLine();
-            
-            Console.WriteLine("W jakim roku się urodziłaś/eś");
-            string sRok = Console.ReadLine();
-            int rok = Convert.ToInt32(sRok);
+            Console.Write("Podaj pozycję menu: ");
 
-            int wiek = DateTime.Now.Year - rok;
+            bool czyKonwersjaSieUdala = int.TryParse(Console.ReadLine(), out int wybor);
 
-            Console.WriteLine($"Witaj {imie}, masz {wiek} lat.");
+            if (czyKonwersjaSieUdala)
+            {
+                int x,y;
+                Console.Write("Podaj X: ");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("Podaj Y: ");
+                y = int.Parse(Console.ReadLine());
+
+
+                switch (wybor)
+                {
+                    case 1:
+                        Console.WriteLine($"Wynik dodawania {x} + {y} to {x + y}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Wynik odejmowania {x} - {y} to {x - y}");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wprowadzono nieprawidłową pozycję menu!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wprowadzono nieprawidłową wartość!");
+                Console.ResetColor();
+            }
         }
 
     }
