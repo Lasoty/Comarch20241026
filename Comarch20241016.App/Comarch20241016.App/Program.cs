fc9bd68b7e3c142d9513b34da3dec21126a16e24
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-10-16 14:29:20 +0200

    Pierwsza aplikacja.

---
+++ Comarch20241016.App/Comarch20241016.App/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ I jest to komentarz
+ wielolinijkowy. 
+ */
+
+using System;
+
+namespace Comarch20241016.App
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Punkt startowy aplikacji
+        /// </summary>
+        /// <param name="args">Parametry startowe aplikacji.</param>
+        /// <returns></returns>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj, jak masz na imię? ");
+
+            string imie;
+            imie = Console.ReadLine();
+            
+            Console.WriteLine("W jakim roku się urodziłaś/eś");
+            string sRok = Console.ReadLine();
+            int rok = Convert.ToInt32(sRok);
+
+            int wiek = DateTime.Now.Year - rok;
+
+            Console.WriteLine($"Witaj {imie}, masz {wiek} lat.");
+        }
+
+    }
+}
