48f42fe7564f9641b672fe25c7388ca9b3e9daed
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-10-17 15:47:06 +0200

    Car manager początek

---
+++ Comarch20241016.App/Comarch20241016.App/CarManager/CarApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comarch20241016.App.CarManager
+{
+    internal class CarApp
+    {
+        public void RunCarManager()
+        {
+            int wybor; 
+            do
+            {
+                PokazMenu();
+                if (int.TryParse(Console.ReadLine(), out wybor))
+                {
+                    UruchomAkcje(wybor);
+                }
+                else
+                {
+                    PokazError("Nieprawidłowa pozycja menu!");
+                }
+
+            } while (wybor != 0);
+        }
+
+        private void UruchomAkcje(int wybor)
+        {
+            switch (wybor)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void PokazError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        private void PokazMenu()
+        {
+            Console.WriteLine("CAR MANAGER 1.0");
+            Console.WriteLine("  1. Lista pojazdów");
+            Console.WriteLine("  2. Dodaj auto");
+            Console.WriteLine("  3. Wypożycz auto");
+            Console.WriteLine("  0. Zakończ");
+        }
+    }
+}
--- Comarch20241016.App/Tests/Comarch20241016.App.Tests/UnitTest1.cs
+++ Comarch20241016.App/Tests/Comarch20241016.App.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Comarch20241016.App.CarManager;
+
 namespace Comarch20241016.App.Tests
 {
     public class ProgramTests
@@ -20,6 +22,12 @@ namespace Comarch20241016.App.Tests
 
             //Assert
             Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        public void TestDostêpnoœci()
+        {
+            Car car = new Car();
+            car.marka = string.Empty;
         }
     }
 }
