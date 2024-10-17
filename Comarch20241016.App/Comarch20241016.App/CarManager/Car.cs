14fc63be0ba07a8280d4855dcb2f096853e85cff
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-10-17 16:05:38 +0200

    CarRepository

---
+++ Comarch20241016.App/Comarch20241016.App/CarManager/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace Comarch20241016.App.CarManager
+{
+    internal class Car
+    {
+        public int Id { get; internal set; }
+
+        public string Marka { get; set; }
+        public string Model { get; internal set; }
+        public int RokProdukcji { get; internal set; }
+    }
+}
---
+++ Comarch20241016.App/Comarch20241016.App/CarManager/CarRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comarch20241016.App.CarManager
+{
+    internal class CarRepository
+    {
+        private List<Car> dane = new List<Car>();
+
+        public CarRepository()
+        {
+            GenerateFakeData();
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            return dane;
+        }
+
+        public Car GetCar(int id)
+        {
+            return dane.FirstOrDefault(car => car.Id == id);
+        }
+
+        public void Add(Car car)
+        {
+            dane.Add(car);
+        }
+
+        private void GenerateFakeData()
+        {
+            int[] tab = { 1, 2, 3, 4, 5, 6 };
+
+            dane = new List<Car>()
+            {
+                new Car()
+                {
+                    Id = 1,
+                    Marka = "Audi",
+                    Model = "A6",
+                    RokProdukcji = 2008
+                },
+
+                new Car()
+                {
+                    Id = 2,
+                    Marka = "BMW",
+                    Model = "M5",
+                    RokProdukcji = 2012
+                },
+
+                new Car()
+                {
+                    Id = 3,
+                    Marka = "Mercedes",
+                    Model = "GLS",
+                    RokProdukcji = 2014
+                }
+            };
+        }
+    }
+}
