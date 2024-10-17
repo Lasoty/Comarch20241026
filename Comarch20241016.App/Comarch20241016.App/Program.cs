ad00dd5e86e7585b29fa6f507ebd28d75ace2104
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-10-17 12:05:36 +0200

    Obsługa wyjątków Testy jednostkowe

--- Comarch20241016.App/Comarch20241016.App/Program.cs
+++ Comarch20241016.App/Comarch20241016.App/Program.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace Comarch20241016.App
 {
-    internal class Program
+    public class Program
     {
 
         /// <summary>
@@ -17,46 +17,99 @@ namespace Comarch20241016.App
         /// <returns></returns>
         static void Main(string[] args)
         {
+            bool czyKontynuowac;
+            do
+            {
+                PokazMenu();
+
+                Console.Write("Podaj pozycję menu: ");
+
+                bool czyKonwersjaSieUdala = int.TryParse(Console.ReadLine(), out int wybor);
+
+                if (czyKonwersjaSieUdala)
+                {
+                    int x, y;
+                    int wynik;
+
+                    switch (wybor)
+                    {
+                        case 1:
+                            PobierzXY(out x, out y);
+                            wynik = CalculateAdd(x, y);
+                            Console.WriteLine($"Wynik dodawania {x} + {y} to {wynik}");
+                            break;
+                        case 2:
+                            PobierzXY(out x, out y);
+                            Console.WriteLine($"Wynik odejmowania {x} - {y} to {x - y}");
+                            break;
+                        case 4:
+                            PobierzXY(out x, out y);
+                            try
+                            {
+                                wynik = CalculateDividy(x, y);
+                                Console.WriteLine($"Wynik dzielenia {x} / {y} to {wynik}");
+                            }
+                            catch (Exception ex)
+                            {
+                                PokazBlad(ex.Message);
+                            }
+                            break;
+                        default:
+                            PokazBlad("Wprowadzono nieprawidłową pozycję menu!");
+                            break;
+                    }
+                }
+                else
+                {
+                    PokazBlad("Wprowadzono nieprawidłową wartość!");
+                }
+                Console.ReadKey();
+
+                Console.Write("Czy chcesz wykonać kolejne działanie? [T|n]");
+                ConsoleKeyInfo consoleKey = Console.ReadKey();
+                czyKontynuowac = consoleKey.Key != ConsoleKey.N;
+
+            } while (czyKontynuowac);
+        }
+
+        private static int CalculateDividy(int x, int y)
+        {
+            if (y == 0)
+                throw new ArgumentException("Pamiętaj cholero! Nie dziel przez 0!");
+
+            return x / y;
+        }
+
+        public static int CalculateAdd(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static void PokazBlad(string wiadomosc)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(wiadomosc);
+            Console.ResetColor();
+        }
+
+        private static void PobierzXY(out int x, out int y)
+        {
+            Console.Clear();
+            Console.Write("Podaj X: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj Y: ");
+            y = int.Parse(Console.ReadLine());
+        }
+
+        private static void PokazMenu()
+        {
+            Console.Clear();
             Console.WriteLine("KALKULATOR 1.0");
             Console.WriteLine("  1. Dodawanie");
             Console.WriteLine("  2. Odejmowanie");
-
-            Console.Write("Podaj pozycję menu: ");
-
-            bool czyKonwersjaSieUdala = int.TryParse(Console.ReadLine(), out int wybor);
-
-            if (czyKonwersjaSieUdala)
-            {
-                int x,y;
-                Console.Write("Podaj X: ");
-                x = int.Parse(Console.ReadLine());
-
-                Console.Write("Podaj Y: ");
-                y = int.Parse(Console.ReadLine());
-
-
-                switch (wybor)
-                {
-                    case 1:
-                        Console.WriteLine($"Wynik dodawania {x} + {y} to {x + y}");
-                        break;
-                    case 2:
-                        Console.WriteLine($"Wynik odejmowania {x} - {y} to {x - y}");
-                        break;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Wprowadzono nieprawidłową pozycję menu!");
-                        Console.ResetColor();
-                        break;
-                }
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wprowadzono nieprawidłową wartość!");
-                Console.ResetColor();
-            }
+            Console.WriteLine("  3. Mnożenie");
+            Console.WriteLine("  4. Dzielenie");
         }
-
     }
 }
---
+++ Comarch20241016.App/Tests/Comarch20241016.App.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace Comarch20241016.App.Tests
+{
+    public class ProgramTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenCalculateAddShouldReturnCorrectValue()
+        {
+            //Arrange
+            int x = 2, y = 3;
+            int actual;
+            int expected = 5;
+            
+            //Act
+            actual = Program.CalculateAdd(x, y);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
